8391c428e6654e8ca48357a06d5b9bbcd10c39ab
hjortie <elinhjortskull@live.se>
2025-02-12 16:03:24 +0100

    added fix for issue 2

--- src/main.ts
+++ src/main.ts
@@ -15,3 +15,32 @@ const jumpings = [1, 4, 56, 32, 7];
 const jumpSum = getLength(jumpings);
 
 console.log(jumpSum);
+
+/*
+    2. I detta exempel har vi fokuserat på if-statements. Se om du kan göra exemplet bättre!
+    */
+
+class Student {
+  constructor(
+    public name: string,
+    public handedInOnTime: boolean,
+    public passed: boolean
+  ) {}
+}
+
+function getStudentStatus(student: Student): string {
+  if (student.handedInOnTime === true) {
+    student.passed = true;
+  }
+
+  if (student.passed) {
+    return "VG";
+  } else {
+    return "IG";
+  }
+}
+
+const elin = new Student("Elin", true, false);
+
+const elinStatus = getStudentStatus(elin);
+console.log(elinStatus);
