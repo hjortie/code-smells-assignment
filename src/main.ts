ae55977095bb77862fc0cc021b6aeeec25c39439
hjortie <elinhjortskull@live.se>
2025-02-12 15:35:43 +0100

    added fix for issue 1

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,17 @@
 import "./style.css";
+
+/*
+  1. Se om du kan hitta två stycken code smells i följande funktion och rätta till dem.
+  Funktionen tar emot en lista med längshoppslängder och syftet med funktionen är att summera
+  dessa hopplängder.
+  */
+
+function getLength(jumpings: number[]): number {
+  return jumpings.reduce(
+    (jumpDistanceSoFar, currentJump) => jumpDistanceSoFar + currentJump
+  );
+}
+const jumpings = [1, 4, 56, 32, 7];
+const jumpSum = getLength(jumpings);
+
+console.log(jumpSum);
